3a0148b7b59cb6f0590c22e41941f60063c6d18d
InsufiKoder <realomrrkn@gmail.com>
2023-05-31 19:49:24 +0300

    add urban

---
+++ commands/urban.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("define")
+    .setDescription("Get the definition of a word from the urban dictionary")
+    .addStringOption((option) =>
+      option
+        .setName("word")
+        .setDescription("Enter a word to define")
+        .setRequired(true)
+    ),
+  global: true,
+  async execute(interaction) {
+    const word = interaction.options.getString("word");
+
+    try {
+      const response = await axios.get(
+        `http://api.urbandictionary.com/v0/define?term=${encodeURIComponent(
+          word
+        )}`
+      );
+      const data = response.data;
+
+      if (data.list && data.list.length > 0) {
+        const [definition] = data.list;
+
+        const replyEmbed = new EmbedBuilder()
+          .setColor("Random")
+          .setTitle("Definition")
+          .addFields({ name: `**${word}**`, value: `${definition.definition}` })
+          .setTimestamp();
+
+        await interaction.reply({ embeds: [replyEmbed] });
+      } else {
+        await interaction.reply({
+          content: `No definition found for **${word}**.`,
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching definition:", error);
+      await interaction.reply({
+        content: "An error occurred while fetching the definition.",
+      });
+    }
+  },
+};
