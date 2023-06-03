eae6215f02aade204ea25c36812f78c224897391
InsufiKoder <realomrrkn@gmail.com>
2023-06-03 15:55:25 +0300

    add echo

---
+++ commands/echo.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("echo")
+    .setDescription("Echoes what the user says")
+    .addStringOption((option) =>
+      option.setName("text").setDescription("Text to echo").setRequired(true)
+    ),
+  global: false,
+  async execute(interaction) {
+    const text = interaction.options.getString("text");
+    try {
+      await interaction.reply({ content: text });
+    } catch (error) {
+      await interaction.reply("An error occured");
+      console.log(error);
+    }
+  },
+};
