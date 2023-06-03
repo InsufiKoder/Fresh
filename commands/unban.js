907464c15dae10e13e55ffadbe0cf51835abf6e2
InsufiKoder <realomrrkn@gmail.com>
2023-06-03 20:19:41 +0300

    add unban

--- commands/unban.js
+++ commands/unban.js
@@ -4,49 +4,54 @@ const { PermissionsBitField } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ban")
-    .setDescription("Ban a user from the server")
-    .addUserOption((option) =>
-      option.setName("user").setDescription("User to ban").setRequired(true)
+    .setName("unban")
+    .setDescription("Unban a user from the server")
+    .addStringOption((option) =>
+      option
+        .setName("userid")
+        .setDescription("The ID of the user to unban")
+        .setRequired(true)
     )
     .addStringOption((option) =>
       option
         .setName("reason")
-        .setDescription("Reason of the ban")
+        .setDescription("Reason for the unban")
         .setRequired(false)
     )
     .setDefaultMemberPermissions(PermissionsBitField.Flags.BanMembers),
   global: true,
   async execute(interaction) {
-    const user = interaction.options.getUser("user");
+    const userId = interaction.options.getString("userid");
     const reason =
       interaction.options.getString("reason") ||
-      `Banned by Fresh. No reason given.`;
+      "Unbanned by Fresh. No reason given.";
 
-    // Check if the bot has the necessary permissions to ban members
+    // Check if the bot has the necessary permissions to unban members
     if (
       !interaction.guild.members.me.permissions.has(
         PermissionsBitField.Flags.BanMembers
       )
     ) {
-      await interaction.reply("I don't have permission to ban members.");
+      await interaction.reply("I don't have permission to unban members.");
       return;
     }
 
-    // Ban the user
+    // Unban the user
     try {
-      await interaction.guild.members.ban(user, { reason: reason });
+      await interaction.guild.members.unban(userId, { reason: reason });
       const replyEmbed = new EmbedBuilder()
         .setColor("Random")
-        .setTitle(`Successfully banned ${user.username}`)
-        .setDescription(`**${user.tag}** has been successfully banned.`)
+        .setTitle(`Successfully unbanned user`)
+        .setDescription(
+          `User with the ID **${userId}** has been successfully unbanned.`
+        )
         .addFields({ name: `\`Reason:\``, value: reason })
         .setTimestamp();
 
       await interaction.reply({ embeds: [replyEmbed] });
     } catch (error) {
-      console.error("Error banning user:", error);
-      await interaction.reply("An error occurred while banning the user.");
+      console.error("Error unbanning user:", error);
+      await interaction.reply("An error occurred while unbanning the user.");
     }
   },
 };
