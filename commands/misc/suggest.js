78e24509915c4079686e840f03b36f783577be02
InsufiKoder <realomrrkn@gmail.com>
2023-06-07 18:53:02 +0300

    move suggest command to it's own file

--- commands/misc/suggest.js
+++ commands/misc/suggest.js
@@ -1,9 +1,51 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("suggestion")
     .setDescription("Suggest a new feature for the bot"),
   global: true,
-  async execute(interaction) {},
+  async execute(interaction) {
+    // Create the modal
+    const suggestionModal = new ModalBuilder()
+      .setCustomId("suggestionModal")
+      .setTitle("Suggestion Form");
+
+    // Add components to modal
+
+    // Create the text input components
+    const suggestionLabelInput = new TextInputBuilder()
+      .setCustomId("suggestionLabelInput")
+      // The label is the prompt the user sees for this input
+      .setLabel("What is your suggestion about?")
+      // Short means only a single line of text
+      .setStyle(TextInputStyle.Short);
+
+    const suggestionInput = new TextInputBuilder()
+      .setCustomId("suggestionInput")
+      .setLabel("What is your suggestion?")
+      // Paragraph means multiple lines of text.
+      .setStyle(TextInputStyle.Paragraph);
+
+    // An action row only holds one text input,
+    // so you need one action row per text input.
+    const firstActionRow = new ActionRowBuilder().addComponents(
+      suggestionLabelInput
+    );
+    const secondActionRow = new ActionRowBuilder().addComponents(
+      suggestionInput
+    );
+
+    // Add inputs to the modal
+    suggestionModal.addComponents(firstActionRow, secondActionRow);
+
+    // Show the modal to the user
+    await interaction.showModal(suggestionModal);
+  },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,12 +1,4 @@
-const {
-  ActionRowBuilder,
-  Events,
-  WebhookClient,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-  EmbedBuilder,
-} = require("discord.js");
+const { Events, WebhookClient, EmbedBuilder } = require("discord.js");
 const { webhookId, webhookToken } = require("../config.json");
 
 const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
@@ -33,43 +25,6 @@ module.exports = {
     }
 
     // Modal Handling
-    if (interaction.commandName == "suggestion") {
-      // Create the modal
-      const suggestionModal = new ModalBuilder()
-        .setCustomId("suggestionModal")
-        .setTitle("Suggestion Form");
-
-      // Add components to modal
-
-      // Create the text input components
-      const suggestionLabelInput = new TextInputBuilder()
-        .setCustomId("suggestionLabelInput")
-        // The label is the prompt the user sees for this input
-        .setLabel("What is your suggestion about?")
-        // Short means only a single line of text
-        .setStyle(TextInputStyle.Short);
-
-      const suggestionInput = new TextInputBuilder()
-        .setCustomId("suggestionInput")
-        .setLabel("What is your suggestion?")
-        // Paragraph means multiple lines of text.
-        .setStyle(TextInputStyle.Paragraph);
-
-      // An action row only holds one text input,
-      // so you need one action row per text input.
-      const firstActionRow = new ActionRowBuilder().addComponents(
-        suggestionLabelInput
-      );
-      const secondActionRow = new ActionRowBuilder().addComponents(
-        suggestionInput
-      );
-
-      // Add inputs to the modal
-      suggestionModal.addComponents(firstActionRow, secondActionRow);
-
-      // Show the modal to the user
-      await interaction.showModal(suggestionModal);
-    }
     // Collect and respond to modals
     if (interaction.isModalSubmit()) {
       if (interaction.customId == "suggestionModal") {
