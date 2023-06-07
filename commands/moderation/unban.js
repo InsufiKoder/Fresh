908258edeb571bea8bb7dd89b6bf48e277a7a3ce
InsufiKoder <realomrrkn@gmail.com>
2023-06-07 18:43:59 +0300

    add buttons to unban

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,6 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder } = require("discord.js");
-const { PermissionsBitField } = require("discord.js");
+const {
+  EmbedBuilder,
+  PermissionsBitField,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ComponentType,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -36,22 +42,69 @@ module.exports = {
       return;
     }
 
-    // Unban the user
-    try {
-      await interaction.guild.members.unban(userId, { reason: reason });
-      const replyEmbed = new EmbedBuilder()
-        .setColor("Random")
-        .setTitle(`Successfully unbanned user`)
-        .setDescription(
-          `User with the ID **${userId}** has been successfully unbanned.`
-        )
-        .addFields({ name: `\`Reason:\``, value: reason })
-        .setTimestamp();
+    const confirm = new ButtonBuilder()
+      .setCustomId("confirmunban")
+      .setLabel("Confirm Unban")
+      .setStyle(ButtonStyle.Danger);
 
-      await interaction.reply({ embeds: [replyEmbed] });
-    } catch (error) {
-      console.error("Error unbanning user:", error);
-      await interaction.reply("An error occurred while unbanning the user.");
+    const cancel = new ButtonBuilder()
+      .setCustomId("cancelunban")
+      .setLabel("Cancel Unban")
+      .setStyle(ButtonStyle.Success);
+
+    const row = new ActionRowBuilder().addComponents(confirm, cancel);
+    const response = await interaction.reply({
+      content: `Are you sure you want to **unban** the user with  the id **${userId}**?`,
+      components: [row],
+    });
+
+    try {
+      const collector = response.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        time: 3_600_000,
+      });
+
+      collector.on("collect", async (i) => {
+        if (i.user.id !== interaction.user.id) {
+          interaction.followUp({
+            content: "Only the command user can use these buttons.",
+            ephemeral: true,
+          });
+          return;
+        }
+        await i.reply("Please wait...");
+
+        if (i.customId === "confirmunban") {
+          await interaction.guild.members.unban(userId, { reason: reason });
+          const replyEmbed = new EmbedBuilder()
+            .setColor("Random")
+            .setTitle(`Successfully unbanned user`)
+            .setDescription(
+              `User with the ID **${userId}** has been successfully unbanned.`
+            )
+            .addFields({ name: `\`Reason:\``, value: reason })
+            .setTimestamp();
+
+          await i.editReply({
+            content: "",
+            embeds: [replyEmbed],
+            components: [],
+          });
+          collector.stop();
+        } else if (i.customId === "cancelunban") {
+          await i.editReply({
+            content: "You cancelled the unban.",
+            components: [],
+          });
+          collector.stop();
+        }
+      });
+    } catch (e) {
+      await interaction.editReply({
+        content: "Confirmation not received within 1 minute, cancelling",
+        components: [],
+      });
+      console.log(e);
     }
   },
 };
