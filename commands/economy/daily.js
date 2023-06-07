76ac4b1d1ec4c212372ca949b5e87c45ca0b2de9
InsufiKoder <realomrrkn@gmail.com>
2023-06-07 22:02:30 +0300

    add multiplier to daily

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 const fs = require("fs");
+
 // Cooldown set
 const recentlyUsed = new Set();
 
@@ -41,15 +42,30 @@ module.exports = {
         return;
       }
 
-      database[interaction.user.id].walletBalance += 1500;
+      const user = database[interaction.user.id];
+      const multiplierItem = user.multiplierItem || false;
+
+      let bonusCoins = 1500;
+      if (multiplierItem) {
+        bonusCoins *= 1.5; // Multiply the bonus coins by 1.5x
+      }
+
+      user.walletBalance += bonusCoins;
 
       // Write the updated database back to the file
       fs.writeFileSync(databasePath, JSON.stringify(database));
 
+      let description;
+      if (!multiplierItem) {
+        description = `You successfully claimed your **${bonusCoins}** daily coins.`;
+      } else if (multiplierItem) {
+        description = `You successfully claimed your **${bonusCoins}** daily coins. \n You made 1.5x extra coins with the 1.5x Multiplier!`;
+      }
+
       const replyEmbed = new EmbedBuilder()
         .setColor("Random")
         .setTitle("Claimed Daily")
-        .setDescription(`You successfully claimed your **1500** daily coins.`)
+        .setDescription(description)
         .setTimestamp();
 
       await interaction.reply({ embeds: [replyEmbed] });
@@ -61,7 +77,7 @@ module.exports = {
     // Adds the user to the set so that they can't use the command for a day
     recentlyUsed.add(interaction.user.id);
     setTimeout(() => {
-      // Removes the user from the set after a minute
+      // Removes the user from the set after a day
       recentlyUsed.delete(interaction.user.id);
     }, 86400000);
   },
