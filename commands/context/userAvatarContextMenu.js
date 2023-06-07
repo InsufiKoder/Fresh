f20b8d07434cf284e266352f0ea3e0058f7fa0ab
InsufiKoder <realomrrkn@gmail.com>
2023-06-07 21:20:08 +0300

    add useravatar

---
+++ commands/context/userAvatarContextMenu.js
@@ -0,0 +1,20 @@
+const { ContextMenuCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder, ApplicationCommandType } = require("discord.js");
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName("Avatar")
+    .setType(ApplicationCommandType.User), // User
+  global: false,
+  async execute(interaction) {
+    const avatar = interaction.targetUser.displayAvatarURL();
+
+    const replyEmbed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle(`${interaction.targetUser.tag}'s Avatar`)
+      .setImage(avatar)
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [replyEmbed] });
+  },
+};
