7697f549fd3af11f9a4c59faad84b7930d4c9028
InsufiKoder <realomrrkn@gmail.com>
2023-06-07 21:24:01 +0300

    add useravatar (non context)

---
+++ commands/misc/userAvatar.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("avatar")
+    .setDescription("Get someone's avatar")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User to get the avatar of")
+        .setRequired(true)
+    ),
+  global: false,
+  async execute(interaction) {
+    const targetUser = interaction.options.getUser("user");
+
+    try {
+      const replyEmbed = new EmbedBuilder()
+        .setColor("Random")
+        .setTitle(`${targetUser.tag}'s Avatar`)
+        .setImage(targetUser.displayAvatarURL())
+        .setTimestamp();
+
+      await interaction.reply({ embeds: [replyEmbed] });
+    } catch (error) {
+      await interaction.reply("An error occured. Please try again later.");
+      console.log(error);
+    }
+  },
+};
